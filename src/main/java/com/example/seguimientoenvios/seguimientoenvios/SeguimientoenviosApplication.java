ee438d2216a3a73447c20be288a815ba2af0a8d5
Haim <haim.pizarro@gmail.com>
2025-03-31 00:45:37 -0300

    Primer Commit

---
+++ src/main/java/com/example/seguimientoenvios/seguimientoenvios/SeguimientoenviosApplication.java
@@ -0,0 +1,13 @@
+package com.example.seguimientoenvios.seguimientoenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguimientoenviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguimientoenviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seguimientoenvios/seguimientoenvios/SeguimientoenviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seguimientoenvios.seguimientoenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguimientoenviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
