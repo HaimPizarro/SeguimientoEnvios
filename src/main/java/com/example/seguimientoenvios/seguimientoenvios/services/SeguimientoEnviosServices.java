52ac79dcd007f4fa30da2299e21eba078b4d14b5
Haim <haim.pizarro@gmail.com>
2025-03-31 23:06:45 -0300

    Finalizacion del prouecto

--- src/main/java/com/example/seguimientoenvios/seguimientoenvios/services/SeguimientoEnviosServices.java
+++ src/main/java/com/example/seguimientoenvios/seguimientoenvios/services/SeguimientoEnviosServices.java
@@ -13,11 +13,11 @@ public class SeguimientoEnviosServices {
     private List<SeguimientoEnvios> seguimientoEnvios = new ArrayList<>();
 
     public SeguimientoEnviosServices() {
-        seguimientoEnvios.add(new SeguimientoEnvios(1L, "2021-01-01", "2021-01-02", "Pendiente", "Prueba", 10, 10, 10));
-        seguimientoEnvios.add(new SeguimientoEnvios(2L, "2021-01-02", "2021-01-03", "Pendiente", "Prueba", 10, 10, 10));
-        seguimientoEnvios.add(new SeguimientoEnvios(3L, "2021-01-03", "2021-01-04", "Pendiente", "Prueba", 10, 10, 10));
-        seguimientoEnvios.add(new SeguimientoEnvios(4L, "2021-01-04", "2021-01-05", "Pendiente", "Prueba", 10, 10, 10));
-        seguimientoEnvios.add(new SeguimientoEnvios(5L, "2021-01-05", "2021-01-06", "Pendiente", "Prueba", 10, 10, 10));
+        seguimientoEnvios.add(new SeguimientoEnvios(1L, "2021-01-01", "2021-01-02", "Pendiente", "Aun no se retira", 3, 1, 10));
+        seguimientoEnvios.add(new SeguimientoEnvios(2L, "2021-01-02", "2021-01-03", "Entregado", "Sin Observaciones", 5, 10, 5));
+        seguimientoEnvios.add(new SeguimientoEnvios(3L, "2021-01-03", "2021-01-04", "En Espera", "Problemas logisticos", 1, 1, 10));
+        seguimientoEnvios.add(new SeguimientoEnvios(4L, "2021-01-04", "2021-01-05", "Con Problemas", "Problemas Logisticos", 24, 12, 10));
+        seguimientoEnvios.add(new SeguimientoEnvios(5L, "2021-01-05", "2021-01-06", "Entregado", "Sin Observaciones", 30, 40, 15));
     }
 
     public List<SeguimientoEnvios> getAll() {
