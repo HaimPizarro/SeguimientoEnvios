a0369bf5bc9900b7ad8ecec885bbefb5af7ec570
Haim <haim.pizarro@gmail.com>
2025-04-15 21:49:30 -0400

    Comenzando modificaciones models

---
+++ src/main/java/com/example/seguimientoenvios/seguimientoenvios/repository/SeguimientoRepository.java
@@ -0,0 +1,7 @@
+package com.example.seguimientoenvios.seguimientoenvios.repository;
+
+public interface SeguimientoRepository extends JpaRepository<ViajeMascota, Long> {
+}
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.viaje.mascota.viajemascota.models.ViajeMascota;
