4d99ba7b2a0ef57313e584fcc2cb98b9c84eba73
HaimPizarro <haim.pizarro@gmail.com>
2025-04-20 16:20:20 -0400

    Configuraciones lista

--- src/main/java/com/example/seguimientoenvios/seguimientoenvios/models/SeguimientoEnvios.java
+++ src/main/java/com/example/seguimientoenvios/seguimientoenvios/models/SeguimientoEnvios.java
@@ -13,7 +13,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
 
 import lombok.Data;
